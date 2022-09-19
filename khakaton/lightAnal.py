eff39e4d2a14473919864ac3e54994bc02464627
archql <59373768+archql@users.noreply.github.com>
2022-09-19 23:34:30 +0300

    imgs filter done
    fog
    dust
    light
    please, setup config b4 continue work with this file!!!

--- khakaton/lightAnal.py
+++ khakaton/lightAnal.py
@@ -1,16 +1,33 @@
-### https://nbviewer.org/github/pytroll/pytroll-examples/blob/main/satpy/satpy_rayleigh_iband_enhanced.ipynb
-### https://satpy.readthedocs.io/en/stable/install.html
-### $ conda create -c conda-forge -n satpy_env python satpy
+import numpy as np
 
-from pyparsing import null_debug_action
-from satpy.scene import Scene
-from satpy import find_files_and_readers
-from glob import glob
-from pathlib import Path
 
-import config
+def value_fun_dust(color_array):
+    r = color_array[2]
+    r = (r - 180) / (255 - 180.)
+    g = color_array[1]
+    g = (255 - g) / (255 - 140.)
 
-### AreaDefinition ### AreaDefinition ### AreaDefinition
-### AreaDefinition ### AreaDefinition ### AreaDefinition
-### AreaDefinition ### AreaDefinition ### AreaDefinition
-# https://github.com/pytroll/satpy/blob/main/satpy/etc/areas.yaml
+    return r * g
+
+
+def value_fun_fog(color_array):
+    return color_array[1] / 255.  # max(color_array[1] - 160, 160) / float(255 - 160)
+
+
+def value_fun_light(color_array):
+    return color_array[1] / 255.
+
+
+def fft_filter(channel, mask):
+    f = np.fft.fft2(channel)
+    fshift = np.fft.fftshift(f)
+
+    # In the lines following, we'll make a copy of the original spectrum and
+    ifshift = np.multiply(fshift, mask)
+
+    ishift = np.fft.ifftshift(ifshift)
+    iimg = np.fft.ifft2(ishift)
+    iimg = np.abs(iimg)
+    imax = iimg.max() / 255.
+    iimg = iimg / imax
+    return iimg.astype(np.uint8)
--- khakaton/main.py
+++ khakaton/main.py
@@ -1,150 +1,105 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 import numpy as np
 import json
-# import matplotlib.pyplot as plt
-# import pandas as pd
-
-# Feature Scaling
-# from sklearn.preprocessing import MinMaxScaler
-
-# neural network
-# f rom tensorflow import keras
-# from keras.optimizers import RMSprop
-# from keras.callbacks import Callback
-# from keras.models import Sequential
-# from keras.layers import LSTM, Dense, Dropout, Bidirectional, SimpleRNN
-
-import matplotlib.pyplot as plt
 
 from pyresample.geometry import AreaDefinition
 import cv2
 
+import numpy as np
+import json
+import matplotlib.pyplot as plt
+from pyresample.geometry import AreaDefinition
+import cv2
 
-def value_fun_light(color_array):
-    return color_array[2]
+import lightAnal as anal
+import config as cfg
 
 
-def tifToJSON(area_definition, image, output_path, value_func, value_threshold):
+def do_masking_light(img):
+    ifshift_mask = cv2.imread(cfg.FULL_PATH + 'testMask.tif', 0)
+    ifshift_mask = ifshift_mask.astype(np.float32) / 255
+
+    channel = img[:, :, 1]
+    channel = anal.fft_filter(channel=channel, mask=ifshift_mask)
+    result_img = np.dstack((channel, channel, channel))  # np.zeros((len(channel), len(channel[0])), dtype=np.uint8)
+
+    return result_img
+
+
+def tif_to_array(image, param):
     """converts an .tif satellite image to an JSON lat, lon, val object.
         Parameters
         ----------
-        area_definition : AreaDefinition
-            AreaDefinition to an .tif image area
-            must be the same size as in area_definition declared (nonscaled)
-        image : something after imread done
+        :param param : dict
+            holds info needed according to parameters
+        :param image : something after imread done
             File name of .tif image (extension included)
-        output_path : string
-            Path to which .json result is saved
-        value_func : function
-            function that converts pixel array (3 values) to value (0..1)
-        value_threshold: double
-            holds a threshold, value is written to a JSON only if greater
+        :return array [['lat', 'lon', 'val']]
         """
+    test_copy = image.copy()
 
     h, w, _ = image.shape
-    lons, lats = area_definition.get_lonlats()
-    jsonResult = []
+    lons, lats = cfg.area.get_lonlats()
 
+    result = []
     for y in range(h):
         for x in range(w):
-            value = value_func(image[y, x])
+            value = param['value_func'](image[y, x])
+            if value > param['threshold']:  # does it work on all data?
+                result.append([
+                    lats[y, x],
+                    lons[y, x],
+                    value
+                ])
+                test_copy[y, x] = [value*255, value*255, value*255]
+            else:
+                test_copy[y, x] = [0, 0, 0]
 
-            if value > value_threshold:
-                jsonObj = {"val": value,
-                           "lat": lats[y, x],
-                           "lon": lons[y, x]}
-                jsonResult.append(jsonObj)
+                # jsonObj = {"val": value,
+                #            "lat": lats[y, x],
+                #            "lon": lons[y, x]}
+                # jsonResult.append(jsonObj)
+    cv2.imwrite(cfg.FULL_PATH + param['name'] + '_ff' + cfg.EXT, test_copy)
+    return result
 
-# to file
-    f = open(output_path + ".json", "w+")
-    f.write(json.dumps(jsonResult))
-    f.close()
-
-
-area_BY = AreaDefinition(
-    area_id='Belarus',
-    description='The region of Belarus (WGS 84 / UTM zone 35N Projection)',
-    projection='EPSG:32635',
-    proj_id='EPSG:32635',
-    width=2800,
-    height=2400,
-    area_extent=(
-        200000,
-        5650000,
-        900000,
-        6250000,
-    ))
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    img = cv2.imread('night_overview.tif')
-
-# do masking (work image out)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
-
-    cv2.fastNlMeansDenoisingColored(img, None, 20, 20, 7, 21)
-
-    lower = np.array([28, 0, 50])  # temp solution
-    upper = np.array([32, 255, 255])
-
-    mask = cv2.inRange(hsv, lower, upper)
-    res = cv2.bitwise_and(img, img, mask=mask)
-# cvt to json
-    tifToJSON(area_definition=area_BY,
-              image=res,
-              output_path='result',
-              value_func=value_fun_light,
-              value_threshold= 120. / 255.)
-    #
-    # cv2.imwrite('night_overview_filtered.tif', cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
-    # cv2.imwrite('night_overview_masked.tif', cv2.cvtColor(res, cv2.COLOR_RGB2BGR))
-    # # cv2.imshow('mask', mask)
-    # # cv2.imshow('res', res)
-    # # cv2.waitKey()
-    #
-    # # _, axarr = plt.subplots(2, 2)
-    # # axarr[0, 0].imshow(img)
-    # # axarr[0, 1].imshow(mask)
-    # # axarr[1, 0].imshow(res)
-    # # plt.show()
-    #
-    # # size down image down by scale
-    # scale = 2.0
-    # scaledW = int(res.shape[1] / scale)
-    # scaledH = int(res.shape[0] / scale)
-    #
-    # # resScaled = res.resize((scaledW, scaledH))
-    # # hmapResScaled = hmap_res.resize((scaledW, scaledH))
-    # # wri res
-    # threshold = 120
-    # hsvResScaled = cv2.cvtColor(res, cv2.COLOR_RGB2HSV)
-    # jsonResult = []
-    #
-    # height, width, _ = res.shape
-    # lons, lats = area_BY.get_lonlats()
-    #
-    # for y in range(height):
-    #     for x in range(width):
-    #         value = hsvResScaled[y, x][2]  # / 255
-    #         valueNorm = value / 255
-    #
-    #         if value > threshold:
-    #             hmap_res[y, x] = [value, value, value]
-    #
-    #             jsonObj = {"val": valueNorm,
-    #                        "lat": lats[y, x],
-    #                        "lon": lons[y, x]}
-    #             jsonResult.append(jsonObj)
-    #         else:
-    #             hmap_res[y, x] = [0, 0, 0]
-    #
-    # cv2.imwrite('hmap.tif', hmap_res)
-    # # to file
-    # f = open("result.json", "w+")
+    # to file
+    # f = open(output_path + ".json", "w+")
     # f.write(json.dumps(jsonResult))
     # f.close()
+
+
+def get_param(param_name):
+    """
+    :param param_name : string
+        'night_overview' -- returns light colors
+    :return: array [['lat', 'lon', 'val']]
+    """
+
+    # get params
+    param = cfg.parameters[param_name]
+    # read image
+    img = cv2.imread(cfg.FULL_PATH + param['name'] + cfg.EXT)
+    # mask image out
+    match param_name:
+        case 'night_overview':
+            # do masking (work image out)
+            img = do_masking_light(img)
+
+    # general masking
+    mask = cv2.inRange(img, param['lower'], param['upper'])
+    img = cv2.bitwise_and(img, img, mask=mask)
+    # save temp mask and res
+    cv2.imwrite(cfg.FULL_PATH + param['name'] + '_mask' + cfg.EXT, mask)
+    cv2.imwrite(cfg.FULL_PATH + param['name'] + '_result' + cfg.EXT, img)
+    # cvt to json
+    return tif_to_array(img, param)
+
+
+# (Example call)
+if __name__ == "__main__":
+
+    # res = get_param('night_overview')
+    # res = get_param('fog')
+    res = get_param('dust')
+
+    for i in range(len(res)):
+        print(res[i])
