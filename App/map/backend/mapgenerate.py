ddaf6b2c5f32dcad53b1b14c6c2b5e84cbccb3e0
KJI0YH <aliaksei.kryzhanouski@gmail.com>
2022-09-19 22:41:01 +0300

    ISS with no internet fix

--- App/map/backend/mapgenerate.py
+++ App/map/backend/mapgenerate.py
@@ -70,51 +70,6 @@ def MapGenerate(filename):
     m.save(filename)
     return m
 
-
-def ISS(ISS_map):
-    path = "https://api.wheretheiss.at/v1/satellites/25544/positions?timestamps="
-    timestamp = int(time.time())
-
-    for i in range(1, 15):
-        path += str(timestamp) + ","
-        timestamp += 450
-
-    path = path[:-1]
-
-    response = requests.get(path)
-    positions = json.loads(response.text)
-
-    # Draw line for ISS markers
-    line_points = pandas.json_normalize(positions)
-    line_points = line_points[['latitude', 'longitude']].values.tolist()
-
-    sections = []
-
-    for i in range(0, len(line_points) - 1):
-        if abs(line_points[i][1] - line_points[i + 1][1]) > 180:
-            sections.append(i + 1)
-
-    lines = np.split(line_points, indices_or_sections=sections)
-
-    for line in lines:
-        ISS_map.add_child(folium.PolyLine(locations=line))
-
-    # Draw ISS markers
-    for pos in positions:
-        ISS_time = datetime.datetime.fromtimestamp(int(pos['timestamp']))
-        tooltip = "International Space Station"
-        tip = "Will be here at " + str(ISS_time)
-
-        if pos['timestamp'] == positions[0]['timestamp']:
-            icon = folium.features.CustomIcon("./map/static/map/img/ISS.png", icon_size=[30, 30])
-            ISS_map.add_child(
-                folium.Marker(location=[pos['latitude'], pos['longitude']], icon=icon, popup=tip, tooltip=tooltip))
-        else:
-            ISS_map.add_child(
-                folium.CircleMarker(location=[pos['latitude'], pos['longitude']], color="blue", fill_color="Blue",
-                                    radius=7, popup=tip, tooltip=tooltip))
-
-
 def LightPollution(light_pollution_map):
 
     # Load points data
@@ -171,6 +126,54 @@ def LightPollution(light_pollution_map):
 
     #light_pollution_map.add_child(folium.plugins.HeatMap(points, radius=10))
 
+def ISS(ISS_map):
+    path = "https://api.wheretheiss.at/v1/satellites/25544/positions?timestamps="
+    timestamp = int(time.time())
+
+    for i in range(1, 15):
+        path += str(timestamp) + ","
+        timestamp += 450
+
+    path = path[:-1]
+
+    try:
+        response = requests.get(path)
+        if response.status_code != 200:
+            return
+    except:
+        return
+
+    positions = json.loads(response.text)
+
+    # Draw line for ISS markers
+    line_points = pandas.json_normalize(positions)
+    line_points = line_points[['latitude', 'longitude']].values.tolist()
+
+    sections = []
+
+    for i in range(0, len(line_points) - 1):
+        if abs(line_points[i][1] - line_points[i + 1][1]) > 180:
+            sections.append(i + 1)
+
+    lines = np.split(line_points, indices_or_sections=sections)
+
+    for line in lines:
+        ISS_map.add_child(folium.PolyLine(locations=line))
+
+    # Draw ISS markers
+    for pos in positions:
+        ISS_time = datetime.datetime.fromtimestamp(int(pos['timestamp']))
+        tooltip = "International Space Station"
+        tip = "Will be here at " + str(ISS_time)
+
+        if pos['timestamp'] == positions[0]['timestamp']:
+            icon = folium.features.CustomIcon("./map/static/map/img/ISS.png", icon_size=[30, 30])
+            ISS_map.add_child(
+                folium.Marker(location=[pos['latitude'], pos['longitude']], icon=icon, popup=tip, tooltip=tooltip))
+        else:
+            ISS_map.add_child(
+                folium.CircleMarker(location=[pos['latitude'], pos['longitude']], color="blue", fill_color="Blue",
+                                    radius=7, popup=tip, tooltip=tooltip))
 
 def flatten(l):
     for el in l:
