fbfd04d988aa7fe88aed408b4a1e31b375fda46a
KJI0YH <aliaksei.kryzhanouski@gmail.com>
2022-09-19 12:30:39 +0300

    fix star map connection

--- App/map/urls.py
+++ App/map/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path("star", views.star, name="star")
+    path("starmap", views.starmap, name="star")
 ]
--- App/map/views.py
+++ App/map/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
 from .backend import mapgenerate as mapgen
-<<<<<<< HEAD
 from django.http import HttpResponse
-=======
 from .backend.starmap_generate import map_generate as starmapgen
 from .backend.mapgenerate import MINSK_LOCATION
-
->>>>>>> f75b0ef7b08b2b874189f57da0bfd39613f43c0f
 
 # Create your views here.
 
@@ -14,21 +10,13 @@ def index(request):
     mapgen.MapGenerate('./map/templates/map/index.html')
     return render(request, 'map/light.html')
 
-<<<<<<< HEAD
-def star(request):
-    return HttpResponse("<h1>star</h1>")
-=======
-
 def starmap(request):
-    # lat = request.GET.get('lat', MINSK_LOCATION[0])
-    # lon = request.Get.get('lon', MINSK_LOCATION[1])
-    lat = request.GET['lat']
-    lon = request.GET['lon']
+    lat = request.GET.get("lat", MINSK_LOCATION[0])
+    lon = request.GET.get("lon", MINSK_LOCATION[1])
     if (not isinstance(lat, float)) or (not isinstance(lon, float)):
         lat = MINSK_LOCATION[0]
         lon = MINSK_LOCATION[1]
     path = starmapgen(lat, lon, './map/templates/starmap/index.html')
-    # path = '/starmap/example.jpg'
+    path = '/starmap/example.jpg'
     data = {"lat": lat, "lon": lon, "path": path}
     return render(request, 'starmap/starmap.html', data)
->>>>>>> f75b0ef7b08b2b874189f57da0bfd39613f43c0f
