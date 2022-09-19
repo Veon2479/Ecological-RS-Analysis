395a03bdbbe8c1ce700b76ce69e5cc51290e2c2e
Whiskeyjack <andriei.shuliak@gmail.com>
2022-09-19 18:28:50 +0300

    fix lat/lon bug

--- App/map/backend/starmap_generate.py
+++ App/map/backend/starmap_generate.py
@@ -1,7 +1,26 @@
 
-
 # working with html page
 def map_generate(lat, lon, filename):
+
+    return 0
+
+
+def get_light_info(lat, lon):
+
+    return 0
+
+
+def get_cloud_info(lat, lon):
+
+    return 0
+
+
+def get_dust_info(lat, lon):
+
+    return 0
+
+
+def get_fog_info(lat, lon):
 
     return 0
 
--- App/map/views.py
+++ App/map/views.py
@@ -4,18 +4,24 @@ from django.http import HttpResponse
 from .backend.starmap_generate import map_generate as starmapgen
 from .backend.mapgenerate import MINSK_LOCATION
 
+
 # Create your views here.
 
 def map(request):
     mapgen.MapGenerate('./map/templates/map/index.html')
     return render(request, 'map/light.html')
 
+
 def starmap(request):
-    lat = request.GET.get("lat", MINSK_LOCATION[0])
-    lon = request.GET.get("lon", MINSK_LOCATION[1])
-    if (not lat.isdigit()) or (not lon.isdigit()):
-        lat = MINSK_LOCATION[0]
-        lon = MINSK_LOCATION[1]
+    lat = 0.0
+    lon = 0.0
+    try:
+        lat = request.GET.get("lat", MINSK_LOCATION[0])
+        lon = request.GET.get("lon", MINSK_LOCATION[1])
+    finally:
+        if (not str(lat).isdigit()) or (not str(lon).isdigit()):
+            lat = MINSK_LOCATION[0]
+            lon = MINSK_LOCATION[1]
     path = starmapgen(lat, lon, './map/templates/starmap/index.html')
     path = '/starmap/example.jpg'
     data = {"lat": lat, "lon": lon, "path": path}
